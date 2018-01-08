9b572f889a5d0a83675eff456ee64c5886ecb121
rafi0005 <32515438+rafi0005@users.noreply.github.com>
2018-01-08 16:28:12 -0500

    filer

---
+++ starter.js
@@ -0,0 +1,5 @@
+let contactStarter = [
+    {"fullname":"mudassar rafique","phone":"613-292-2456","email":"m.rafique@gmail.com"},
+    {"fullname":"ali hassan","phone":"613-858-8899","email":"ali.h@gmail.com"},
+    {"fullname":"david wood","phone":"413-789-8990","email":"david.wood@gmail.com"}
+];
